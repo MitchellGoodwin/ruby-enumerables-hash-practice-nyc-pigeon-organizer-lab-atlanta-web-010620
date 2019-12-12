b89fb55b8ff856d23f9800e5bfb3118b78ed6864
Mitchell Goodwin <mgoodwin777@gmail.com>
2019-12-12 20:02:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,5 +2,5 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   result = {}
   names = data[:gender][:male] >> data[:gender][:female]
-  result = names.map{ |name| name => {:color => [], :gender => [], :lives => []}
+  result = names.map{ |name| name = name => {:color => [], :gender => [], :lives => []}
 end
