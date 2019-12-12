8448280d27e02fa828cf301fed30584e4aef2344
Mitchell Goodwin <mgoodwin777@gmail.com>
2019-12-12 20:57:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,13 +4,10 @@ def nyc_pigeon_organizer(data)
   names = data[:gender][:male] + data[:gender][:female]
   result = Hash.new
   names.each { |name| result[name] = {:color => [], :gender => [], :lives => []}}
-  #data[:color].each do |memo, (key, value)|
-    #value.each { |name| result[name][:color].push(key.to_s)}
-    #memo
- #end
   data[:color].each do |(key, value)|
     value.each { |name| result[name][:color].push("#{key}")}
   end
+  
   print result
   result
 end
