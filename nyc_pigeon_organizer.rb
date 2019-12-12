7cfffe778273723780a25dac506c75568ca2066e
Mitchell Goodwin <mgoodwin777@gmail.com>
2019-12-12 20:41:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,9 @@ def nyc_pigeon_organizer(data)
   result = Hash.new
   names.each { |name| result[name] = {:color => [], :gender => [], :lives => []}}
   pigeon_data[:color].each do |memo, (key, value)|
-    value.each { |name| result[name][:color] <<
+    value.each { |name| result[name][:color] << key.to_s}
+    memo
+  end
   print result
   result
 end
